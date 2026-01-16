8f50655fb3803919634f0e8c99802ad28fae650d
Andriy <andrijbuluk@gmail.com>
2026-01-16 19:04:22 +0100

    Copy file Fix Errors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
-def copy_file(command):
+def copy_file(command) -> None:
     cp, src, dst = command.split()
 
     if src == dst:
+        return
+
+    if command != 3:
         return
 
     with open(src, "r") as file_in, open(dst, "w") as file_out:
