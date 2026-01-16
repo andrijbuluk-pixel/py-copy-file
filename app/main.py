927bde114d7c65111d0b1a747c9dbaad97df9a2b
Andriy <andrijbuluk@gmail.com>
2026-01-16 19:11:58 +0100

    Copy file Fix Errors audit

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
+from _pyrepl import commands
+
+
 def copy_file(command) -> None:
     cp, src, dst = command.split()
+    command_split = command.split()
+
+    for c in command_split:
+        if c == 3:
+            return
+        elif c != "cp":
+            return
 
     if src == dst:
-        return
-
-    if command != 3:
         return
 
     with open(src, "r") as file_in, open(dst, "w") as file_out:
